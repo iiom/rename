3b26ee41af4ad10e6fe04217ba9ac420e218eb30
iiom <ioma766@gmail.com>
2018-11-28 02:23:43 +0300

    first commit

---
+++ lib/rename.rb
@@ -0,0 +1,34 @@
+require 'fileutils'
+
+current_path = File.dirname(__FILE__)
+file_in_dir = Dir.entries(current_path)
+file_in_dir.shift(2)
+dest = current_path + "/copy"
+
+file_in_dir.each do |f|
+  if File.extname(f) == ".sample"
+    FileUtils.cp(f, dest)
+    File.rename(File.basename(f), File.basename(f, ".*"))
+  end
+end
+
+
+
+# basename
+# Если вы имеете дело с файлами, вы можете использовать basename
+# File.basename("/home/gumby/work/ruby.rb")   #=> "ruby.rb"
+# File.basename("/home/gumby/work/ruby.rb", ".rb") #=> "ruby"
+#
+# В противном случае, если это просто строка:
+# 'C:/EFI/Ulta/Filename.rb'.split('/').last.split('.').first
+#
+# File.basename(file, '.*')[0,3] обрезает имя файла до 3х символов
+#
+# extname
+# Returns the extension (the portion of file name in path after the period).
+# File.extname("test.rb")         #=> ".rb"
+# File.extname("a/b/d/test.rb")   #=> ".rb"
+# File.extname("test")            #=> ""
+# File.extname(".profile")        #=> ""
+#
+#
