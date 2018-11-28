0439aa98db4416c34acca5ea1b6eed399661d7bc
iiom <ioma766@gmail.com>
2018-11-28 15:04:46 +0300

    +copy&rename in current folder

--- lib/rename.rb
+++ lib/rename.rb
@@ -3,11 +3,10 @@ require 'fileutils'
 current_path = File.dirname(__FILE__)
 file_in_dir = Dir.entries(current_path)
 file_in_dir.shift(2)
-dest = current_path + "/copy"
 
 file_in_dir.each do |f|
   if File.extname(f) == ".sample"
-    FileUtils.cp(f, dest)
+    FileUtils.cp(f, File.basename(f, ".sample") + "-copy" + File.extname(f))
     File.rename(File.basename(f), File.basename(f, ".*"))
   end
 end
